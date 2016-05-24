ffe55065c44c2d914b1661b5c187927afdd25d25
Konstantin Androsov <konstantin.androsov@gmail.com>
2016-05-24 20:14:05 +0200

    Added KinFitResults to SyncEventInfo.

--- Analysis/include/FlatEventInfo.h
+++ Analysis/include/FlatEventInfo.h
@@ -3,14 +3,20 @@ This file is part of https://github.com/hh-italian-group/h-tautau. */
 
 #pragma once
 
-#include <TLorentzVector.h>
-#include <TMatrixD.h>
-
 #include "SyncTree.h"
 #include "AnalysisTypes.h"
-#include "exception.h"
+#include "RootExt.h"
+#include "HHKinFit2/HHKinFit2/interface/HHKinFitMasterHeavyHiggs.h"
 
 namespace analysis {
+
+struct KinFitResult {
+    double mass, chi2, probability;
+    int convergence;
+    bool HasMass() const { return convergence > 0; }
+
+    KinFitResult() : convergence(std::numeric_limits<int>::lowest()) {}
+};
 
 struct SyncEventInfo {
     typedef std::pair<size_t, size_t> BjetPair;
@@ -44,47 +50,76 @@ struct SyncEventInfo {
         }
     }
 
- const ntuple::Sync* event;
- analysis::Channel channel;
- std::vector<TLorentzVector> lepton_momentums;
- std::vector<TLorentzVector> bjet_momentums;
- BjetPair selected_bjets;
- bool has_bjet_pair;
- Float_t npu;
- Int_t npv;
- TLorentzVector MET, Htt, Htt_MET, Hbb, resonance;
- TMatrixD MET_covariance;
+    static constexpr int verbosity = 0;
+    const ntuple::Sync* event;
+    analysis::Channel channel;
+    std::vector<TLorentzVector> lepton_momentums;
+    std::vector<TLorentzVector> bjet_momentums;
+    BjetPair selected_bjets;
+    bool has_bjet_pair;
+    TLorentzVector MET, Htt, Htt_MET, Hbb, resonance;
+    TMatrixD MET_covariance;
 
- SyncEventInfo(const ntuple::Sync& _event, const BjetPair& _selected_bjets)
-     : event(&_event),
-       channel(static_cast<analysis::Channel>(analysis::Channel::MuTau)),
-       lepton_momentums(2), bjet_momentums(_event.pt_jets.size()), selected_bjets(_selected_bjets),
-       has_bjet_pair(false), npu(-1), npv(-1), MET_covariance(2, 2)
- {
-     lepton_momentums.at(0).SetPtEtaPhiM(event->pt_1, event->eta_1, event->phi_1, event->m_1);
-     lepton_momentums.at(1).SetPtEtaPhiM(event->pt_2, event->eta_2, event->phi_2, event->m_2);
-     npu = event->npu;
-     npv = event->npv;
 
-     for(size_t n = 0; n < bjet_momentums.size(); ++n)
-         bjet_momentums.at(n).SetPtEtaPhiE(event->pt_jets.at(n), event->eta_jets.at(n), event->phi_jets.at(n),
-                                           event->energy_jets.at(n));
+    SyncEventInfo(const ntuple::Sync& _event, const BjetPair& _selected_bjets)
+        : event(&_event), channel(static_cast<analysis::Channel>(analysis::Channel::MuTau)),
+          lepton_momentums(2), bjet_momentums(_event.pt_jets.size()), selected_bjets(_selected_bjets),
+          has_bjet_pair(false), MET_covariance(2, 2)
+    {
+        lepton_momentums.at(0).SetPtEtaPhiM(event->pt_1, event->eta_1, event->phi_1, event->m_1);
+        lepton_momentums.at(1).SetPtEtaPhiM(event->pt_2, event->eta_2, event->phi_2, event->m_2);
 
-     MET.SetPtEtaPhiM(event->met, 0, event->metphi, 0);
-     MET_covariance(0, 0) = event->metcov00;
-     MET_covariance(1, 0) = event->metcov10;
-     MET_covariance(0, 1) = event->metcov01;
-     MET_covariance(1, 1) = event->metcov11;
+        for(size_t n = 0; n < bjet_momentums.size(); ++n)
+            bjet_momentums.at(n).SetPtEtaPhiE(event->pt_jets.at(n), event->eta_jets.at(n), event->phi_jets.at(n),
+        event->energy_jets.at(n));
 
-     Htt = lepton_momentums.at(0) + lepton_momentums.at(1);
-     Htt_MET = Htt + MET;
-     has_bjet_pair = selected_bjets.first < bjet_momentums.size() && selected_bjets.second < bjet_momentums.size();
-     if(has_bjet_pair) {
-         Hbb = bjet_momentums.at(selected_bjets.first) + bjet_momentums.at(selected_bjets.second);
-         resonance = Htt_MET + Hbb;
-     }
- }
+        MET.SetPtEtaPhiM(event->met, 0, event->metphi, 0);
+        MET_covariance(0, 0) = event->metcov00;
+        MET_covariance(1, 0) = event->metcov10;
+        MET_covariance(0, 1) = event->metcov01;
+        MET_covariance(1, 1) = event->metcov11;
 
+        Htt = lepton_momentums.at(0) + lepton_momentums.at(1);
+        Htt_MET = Htt + MET;
+        has_bjet_pair = selected_bjets.first < bjet_momentums.size() && selected_bjets.second < bjet_momentums.size();
+        if(has_bjet_pair) {
+            Hbb = bjet_momentums.at(selected_bjets.first) + bjet_momentums.at(selected_bjets.second);
+            resonance = Htt_MET + Hbb;
+        }
+    }
+
+    const KinFitResult& GetKinFitResults() const
+    {
+        if(!kinFit_result) {
+            kinFit_result = std::shared_ptr<KinFitResult>(new KinFitResult());
+            if(has_bjet_pair) {
+                if(verbosity > 1) {
+                    std::cout << "b1: " << bjet_momentums.at(selected_bjets.first)
+                              << "\nb2:" << bjet_momentums.at(selected_bjets.second)
+                              << "\ntau1:" << lepton_momentums.at(0)
+                              << "\ntau2:" << lepton_momentums.at(1)
+                              << "\nMET: (" << MET.X() << ", " << MET.Y() << ")"
+                              << "\nMET cov:" << MET_covariance << std::endl;
+                }
+                HHKinFit2::HHKinFitMasterHeavyHiggs kin_fit(bjet_momentums.at(selected_bjets.first),
+                                                            bjet_momentums.at(selected_bjets.second),
+                                                            lepton_momentums.at(0), lepton_momentums.at(1),
+                                                            TVector2(MET.X(), MET.Y()), MET_covariance);
+                kin_fit.verbosity = verbosity;
+                kin_fit.fit();
+                kinFit_result->convergence = kin_fit.getConvergence();
+                if(kinFit_result->HasMass()) {
+                    kinFit_result->mass = kin_fit.getMH();
+                    kinFit_result->chi2 = kin_fit.getChi2();
+                    kinFit_result->probability = kin_fit.getFitProb();
+                }
+            }
+        }
+        return *kinFit_result;
+    }
+
+private:
+    mutable std::shared_ptr<KinFitResult> kinFit_result;
 };
 
 using SyncEventInfoPtr = std::shared_ptr<SyncEventInfo>;
