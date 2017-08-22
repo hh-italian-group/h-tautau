4ed760a265793c18d6a07fca695c4b2605e26b20
Konstantin Androsov <kandrosov@users.noreply.github.com>
2017-08-22 16:06:48 +0300

    Updated b tag WP values
    Used WP defined for ReReco data in https://twiki.cern.ch/twiki/bin/viewauth/CMS/BtagRecommendation80XReReco

--- Cuts/include/Btag_2016.h
+++ Cuts/include/Btag_2016.h
@@ -1,15 +1,15 @@
 /*! b-jet selection recommended by b-tag POG.
 If not specified otherwise, all definitions are taken from the b-tag POG 80X recommendation TWiki:
-https://twiki.cern.ch/twiki/bin/viewauth/CMS/BtagRecommendation80X.
+https://twiki.cern.ch/twiki/bin/viewauth/CMS/BtagRecommendation80XReReco.
 This file is part of https://github.com/hh-italian-group/h-tautau. */
 
 #pragma once
 
 namespace cuts {
 namespace btag_2016 {
-    constexpr double CSVv2L = 0.460; // >
-    constexpr double CSVv2M = 0.800; // >
-    constexpr double CSVv2T = 0.935; // >
+    constexpr double CSVv2L = 0.5426; // >
+    constexpr double CSVv2M = 0.8484; // >
+    constexpr double CSVv2T = 0.9535; // >
 
     constexpr double pt = 20; // >
     constexpr double eta = 2.4; // <
