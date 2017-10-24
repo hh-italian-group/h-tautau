ec17b4ef2157732a431d79cda3d4f87bf431be89
Maria Teresa <grippomariateresa@gmail.com>
2017-10-24 14:52:38 +0200

    - McCorrections/source/PileUpCalc.cxx: modified to read more MC files   and changed npu binning

--- McCorrections/source/PileUpCalc.cxx
+++ McCorrections/source/PileUpCalc.cxx
@@ -11,7 +11,7 @@ This file is part of https://github.com/hh-italian-group/hh-bbtautau. */
 
 struct Arguments {
     run::Argument<std::string> tree_name{"tree_name", "Tree on which we work"};
-    run::Argument<std::string> MC_input_file{"MC_input_file", "MC input file"};
+    run::Argument<std::vector<std::string>> MC_input_files{"MC_input_files", "MC input files"};
     run::Argument<std::string> data_pileup_file{"data_pileup_file", "Pileup file for data"};
     run::Argument<std::string> output_weight_file{"output_weight_file", "Output weight root file"};
 };
@@ -21,8 +21,8 @@ namespace analysis {
 class PileUpCalcData : public root_ext::AnalyzerData {
 public:
     using AnalyzerData::AnalyzerData;
-    TH1D_ENTRY(n_pu_mc, 200, 0, 200)
-    TH1D_ENTRY(pileup, 200, 0, 200)
+    TH1D_ENTRY(n_pu_mc, 1000, 0, 100)
+    TH1D_ENTRY(pileup, 1000, 0, 100)
 };
 
 
@@ -31,30 +31,31 @@ public:
     PileUpCalc(const Arguments& _args) :
         args(_args), output(root_ext::CreateRootFile(args.output_weight_file())), anaData(output)
     {
-        LoadInputs();
+
     }
 
-    void Run() {}
+    void Run()
+    {
+        for(const auto& file_name : args.MC_input_files()){
+            auto inputFile = root_ext::OpenRootFile(file_name);
+            auto data_pileup_file = root_ext::OpenRootFile(args.data_pileup_file());
+            ntuple::ExpressTuple tuple(args.tree_name(), inputFile.get(), true);
+            if(!tuple) continue;
+            for(const auto& event : tuple)
+                anaData.n_pu_mc(file_name).Fill(event.npu);
+
+            auto n_pu_data = std::shared_ptr<TH1D>(root_ext::ReadObject<TH1D>(*data_pileup_file, "pileup"));
+            anaData.pileup().CopyContent(*n_pu_data);
+            RenormalizeHistogram(anaData.pileup(), 1, true);
+            RenormalizeHistogram(anaData.n_pu_mc(file_name), 1, true);
+            //anaData.pileup().Divide(&anaData.n_pu_mc());
+        }
+    }
 
 private:
     Arguments args;
     std::shared_ptr<TFile> output;
     PileUpCalcData anaData;
-
-    void LoadInputs()
-    {
-        auto inputFile = root_ext::OpenRootFile(args.MC_input_file());
-        auto data_pileup_file = root_ext::OpenRootFile(args.data_pileup_file());
-        ntuple::ExpressTuple tuple(args.tree_name(), inputFile.get(), true);
-        for(const auto& event : tuple)
-            anaData.n_pu_mc().Fill(event.npu);
-
-        auto n_pu_data = std::shared_ptr<TH1D>(root_ext::ReadObject<TH1D>(*data_pileup_file, "pileup"));
-        anaData.pileup().CopyContent(*n_pu_data);
-        RenormalizeHistogram(anaData.pileup(), 1, true);
-        RenormalizeHistogram(anaData.n_pu_mc(), 1, true);
-        anaData.pileup().Divide(&anaData.n_pu_mc());
-    }
 };
 
 } //namespace analysis
