271132de69ceed48f42d6586d11508c6ac9f3af2
rbhattacharya04 <rajarshi.bhattacharya@cern.ch>
2017-10-25 20:40:55 +0200

    Name of the new file has been added

--- McCorrections/include/EventWeights.h
+++ McCorrections/include/EventWeights.h
@@ -39,7 +39,7 @@ public:
                         FullLeptonName("Muon/Run2016BtoH/Muon_IdIso_IsoLt0p2_2016BtoH_eff_update1407.root"),
                         FullLeptonName("Muon/Run2016BtoH/Muon_Mu22OR_eta2p1_eff.root"));
             providers[WeightType::BTag] = std::make_shared<BTagWeight>(
-                        FullName("bTagEfficiencies_80X.root"), FullName("CSVv2_Moriond17_B_H.csv"), btag_wp);
+                        FullName("bTagEfficiencies_Moriond17.root"), FullName("CSVv2_Moriond17_B_H.csv"), btag_wp);
 			
             providers[WeightType::TopPt] = std::make_shared<TopPtWeight>(0.0615, 0.0005);
 
