847008ff24bead46be7b05faa80170271ce1fb5c
Maria Teresa <grippomariateresa@gmail.com>
2017-10-26 09:22:53 +0200

    - renamed:    McCorrections/source/PileUpCalc.cxx ->   McCorrections/source/PUcalc.cxx and new calculation of PU renormalizing in a finite range - McCorrections/test/PileUpCalc_t.cxx: added Enabled branches and chi2   and KS test

--- McCorrections/source/PUcalc.cxx
+++ McCorrections/source/PUcalc.cxx
@@ -12,6 +12,7 @@ This file is part of https://github.com/hh-italian-group/hh-bbtautau. */
 struct Arguments {
     run::Argument<std::string> MC_input_file{"MC_input_file", "MC input file"};
     run::Argument<std::string> data_pileup_file{"data_pileup_file", "Pileup file for data"};
+    run::Argument<int> max_n_pu{"max_n_pu","maximum value for n_pu"};
     run::Argument<std::string> output_weight_file{"output_weight_file", "Output weight root file"};
 
 };
@@ -21,7 +22,7 @@ namespace analysis {
 class PileUpCalcData : public root_ext::AnalyzerData {
 public:
     using AnalyzerData::AnalyzerData;
-    TH1D_ENTRY(pileup, 1000, 0, 100)
+    ANA_DATA_ENTRY(TH1D, pileup)
 };
 
 
@@ -38,25 +39,47 @@ public:
 
         auto mc_pileup_file = root_ext::OpenRootFile(args.MC_input_file());
         auto data_pileup_file = root_ext::OpenRootFile(args.data_pileup_file());
+        auto pu_data = std::shared_ptr<TH1D>(root_ext::ReadObject<TH1D>(*data_pileup_file, "pileup"));
+        anaData.pileup.SetMasterHist(pu_data->GetNbinsX(),pu_data->GetXaxis()->GetBinUpEdge(0),
+                                     pu_data->GetXaxis()->GetBinLowEdge(pu_data->GetNbinsX()));
+
         auto pu_mc = std::shared_ptr<TH1D>(root_ext::ReadObject<TH1D>(*mc_pileup_file, "n_pu_mc"));
         anaData.pileup("mc").CopyContent(*pu_mc);
-
         anaData.pileup("mc_norm").CopyContent(anaData.pileup("mc"));
-        const double integral_mc = anaData.pileup("mc_norm").Integral(1,34);
+        const int max_bin = anaData.pileup().FindBin(args.max_n_pu());
+        anaData.pileup("data").CopyContent(*pu_data);
+        anaData.pileup("data_norm").CopyContent(anaData.pileup("data"));
+
+        for(unsigned i = 0; i < anaData.pileup().GetNbinsX(); ++i){
+            if(i<=max_bin) continue;
+            anaData.pileup("mc_norm").SetBinContent(i,0);
+            anaData.pileup("data_norm").SetBinContent(i,0);
+        }
+
+        const double integral_mc = anaData.pileup("mc_norm").Integral(1,max_bin);
         if (integral_mc == 0)
             throw analysis::exception("Integral mc is zero.");
         anaData.pileup("mc_norm").Scale(1 /integral_mc);
 
+        anaData.pileup("mc_norm_full").CopyContent(anaData.pileup("mc"));
+        RenormalizeHistogram(anaData.pileup("mc_norm_full"), 1, true);
 
-        auto pu_data = std::shared_ptr<TH1D>(root_ext::ReadObject<TH1D>(*data_pileup_file, "pileup"));
-        anaData.pileup("data").CopyContent(*pu_data);
-        anaData.pileup("data_norm").CopyContent(anaData.pileup("data"));
-        const double integral_data = anaData.pileup("data_norm").Integral(1,34);
+
+        const double integral_data = anaData.pileup("data_norm").Integral(1,max_bin);
         if (integral_data == 0)
             throw analysis::exception("Integral data is zero.");
         anaData.pileup("data_norm").Scale(1 /integral_data);
 
+        anaData.pileup("data_norm_full").CopyContent(anaData.pileup("data"));
+        RenormalizeHistogram(anaData.pileup("data_norm_full"), 1, true);
+
         anaData.pileup("weight").CopyContent(anaData.pileup("data_norm"));
+
+        for(unsigned i = 0; i < anaData.pileup("weight").GetNbinsX(); ++i){
+            if(i<=max_bin) continue;
+            anaData.pileup("weight").SetBinContent(i,0);
+        }
+
         anaData.pileup("weight").Divide(&anaData.pileup("mc_norm"));
 
     }
--- McCorrections/test/PileUpCalc_t.cxx
+++ McCorrections/test/PileUpCalc_t.cxx
@@ -24,7 +24,7 @@ namespace analysis {
 class PileUpCalcData : public root_ext::AnalyzerData {
 public:
     using AnalyzerData::AnalyzerData;
-    TH1D_ENTRY(npv, 1000, 0, 100)
+    TH1D_ENTRY(npv, 100, 0, 100)
 };
 
 class PileUpCalc_t {
@@ -43,8 +43,10 @@ public:
     {
         auto inputFile_mc = root_ext::OpenRootFile(args.input_MC_file());
         auto inputFile_data = root_ext::OpenRootFile(args.input_data_file());
-        auto eventTuple_mc = ntuple::CreateEventTuple(args.tree_name(),inputFile_mc.get(),true,ntuple::TreeState::Full);
-        auto eventTuple_data = ntuple::CreateEventTuple(args.tree_name(),inputFile_data.get(),true,ntuple::TreeState::Full);
+        auto eventTuple_mc =
+                ntuple::CreateEventTuple(args.tree_name(),inputFile_mc.get(),true,ntuple::TreeState::Full,GetEnabledBranches());
+        auto eventTuple_data =
+                ntuple::CreateEventTuple(args.tree_name(),inputFile_data.get(),true,ntuple::TreeState::Full,GetEnabledBranches());
 
         for(const ntuple::Event& event : *eventTuple_mc) {
             anaData.npv("mc").Fill(event.npv, pu_weight.Get(event));
@@ -54,6 +56,10 @@ public:
             anaData.npv("data").Fill(event.npv, pu_weight.Get(event));
         }
 
+        const double chi2_test = anaData.npv("mc").Chi2Test(&anaData.npv("data"), "WW");
+        const double ks_test = anaData.npv("mc").KolmogorovTest(&anaData.npv("data"));
+
+        std::cout << "Chi2 test: " << chi2_test << ", KS test: " << ks_test << std::endl;
     }
 
 
@@ -62,6 +68,14 @@ private:
     PileUpWeight pu_weight;
     std::shared_ptr<TFile> output;
     PileUpCalcData anaData;
+
+    static const std::set<std::string>& GetEnabledBranches()
+    {
+        static const std::set<std::string> EnabledBranches_read = {
+            "npv", "npu"
+        };
+        return EnabledBranches_read;
+    }
 
 
 };
