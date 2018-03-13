0d6f660530c6def1c99f7709f5f6b4342336a45d
Maria Teresa <grippomariateresa@gmail.com>
2018-03-13 16:46:42 +0100

    - Cuts/include/hh_bbtautau_2017.h: loosened pt cuts for ele mu and tau   in all 3 channels, according to the trigger match implementation

--- Cuts/include/hh_bbtautau_2017.h
+++ Cuts/include/hh_bbtautau_2017.h
@@ -11,14 +11,16 @@ constexpr double DeltaR_betweenSignalObjects = 0.1; // > Decreased to not loose 
 
 namespace MuTau {
     namespace muonID {
-        constexpr double pt = 23; // > Increased to be away from the trigger threshold.
+        //constexpr double pt = 23; // > Increased to be away from the trigger threshold.
+        constexpr double pt = 10; // > Increased to be away from the trigger threshold.
         constexpr bool isTightMuon = true; // = Should be ok, since the HIP problem is solved.
 
         // pfRelIso should be applied at the tuple production level.
     }
 
     namespace tauID {
-        constexpr double pt = 25; // > to be fixed
+        //constexpr double pt = 25; // > to be fixed
+        constexpr double pt = 20; // > to be fixed
         // againstElectronVLooseMVA6 and againstMuonTight3 should be applied at the tuple production level.
     }
 
@@ -27,7 +29,8 @@ namespace MuTau {
 
 namespace ETau {
     namespace electronID {
-        constexpr double pt = 27; // > Increased to be away from the trigger threshold.
+        //constexpr double pt = 27; // > Increased to be away from the trigger threshold.
+        constexpr double pt = 10; // > Increased to be away from the trigger threshold.
 
         // missingHits and passConversionVeto should not be applied, because they are used as inputs to the MVA id and
         // direct cut on them is not recommented by POG.
@@ -36,7 +39,8 @@ namespace ETau {
     }
 
     namespace tauID {
-        constexpr double pt = 35; // > to be fixed
+        //constexpr double pt = 35; // > to be fixed
+        constexpr double pt = 20; // > to be fixed
         // againstElectronTightMVA6 and againstMuonLoose3 should be applied at the tuple production level.
     }
 
@@ -45,7 +49,8 @@ namespace ETau {
     
 namespace TauTau {
     namespace tauID {
-        constexpr double pt = 45; // > to be fixed
+        //constexpr double pt = 45; // > to be fixed
+        constexpr double pt = 20; // > to be fixed
         constexpr double eta = 2.3; // > to be fixed
     }
 }
