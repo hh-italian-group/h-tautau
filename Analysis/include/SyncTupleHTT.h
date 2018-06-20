192f8d8257f7833fc2bd0a8e5ce409399b98616f
Maria Teresa Grippo <mgrippo@users.noreply.github.com>
2018-06-20 17:18:01 +0200

    Sync - mva score (#55)
    Added mva score variables for central and up/down ES in the syncTuple producer.

--- Analysis/include/SyncTupleHTT.h
+++ Analysis/include/SyncTupleHTT.h
@@ -9,6 +9,8 @@ This file is part of https://github.com/hh-italian-group/h-tautau. */
 #include "h-tautau/Cuts/include/Btag_2016.h"
 #include "h-tautau/Cuts/include/Btag_2017.h"
 #include "AnalysisTypes.h"
+#include "hh-bbtautau/Analysis/include/MvaReader.h"
+#include "hh-bbtautau/Analysis/include/SampleDescriptorConfigEntryReader.h"
 
 #define LVAR(type, name, pref) VAR(type, name##_##pref)
 #define JVAR(type, name, suff, pref) VAR(type, suff##name##_##pref)
@@ -148,11 +150,33 @@ This file is part of https://github.com/hh-italian-group/h-tautau. */
     JET_DATA(bjet_, 1) /* leading b-jet sorted by csv (Fill only if corrected b-jet pt>20 GeV) */ \
     JET_DATA(bjet_, 2) /* leading b-jet sorted by csv (Fill only if corrected b-jet pt>20 GeV) */ \
     VAR(Double_t, ht_other_jets) /* Ht of all jets in the event except the first 2 jets */\
+    /* mva_score */ \
+    VAR(Double_t, mva_score_nonRes_kl1) /* mva_score */\
+    VAR(Double_t, mva_score_lm_320) /* mva_score */\
+    VAR(Double_t, mva_score_mm_400) /* mva_score */\
+    VAR(Double_t, mva_score_hm_650) /* mva_score */\
+    VAR(Double_t, mva_score_nonRes_kl1_tau_ES_up) /* mva_score */\
+    VAR(Double_t, mva_score_lm_320_tau_ES_up) /* mva_score */\
+    VAR(Double_t, mva_score_mm_400_tau_ES_up) /* mva_score */\
+    VAR(Double_t, mva_score_hm_650_tau_ES_up) /* mva_score */\
+    VAR(Double_t, mva_score_nonRes_kl1_tau_ES_down) /* mva_score */\
+    VAR(Double_t, mva_score_lm_320_tau_ES_down) /* mva_score */\
+    VAR(Double_t, mva_score_mm_400_tau_ES_down) /* mva_score */\
+    VAR(Double_t, mva_score_hm_650_tau_ES_down) /* mva_score */\
+    VAR(Double_t, mva_score_nonRes_kl1_jet_ES_up) /* mva_score */\
+    VAR(Double_t, mva_score_lm_320_jet_ES_up) /* mva_score */\
+    VAR(Double_t, mva_score_mm_400_jet_ES_up) /* mva_score */\
+    VAR(Double_t, mva_score_hm_650_jet_ES_up) /* mva_score */\
+    VAR(Double_t, mva_score_nonRes_kl1_jet_ES_down) /* mva_score */\
+    VAR(Double_t, mva_score_lm_320_jet_ES_down) /* mva_score */\
+    VAR(Double_t, mva_score_mm_400_jet_ES_down) /* mva_score */\
+    VAR(Double_t, mva_score_hm_650_jet_ES_down) /* mva_score */\
+    /* m_kinFit */ \
     VAR(Float_t, m_kinfit) \
     VAR(Float_t, m_kinfit_tau_ES_up) \
     VAR(Float_t, m_kinfit_tau_ES_down) \
     VAR(Float_t, m_kinfit_jet_ES_up) \
-    VAR(Float_t, m_kinfit_jet_ES_down ) \
+    VAR(Float_t, m_kinfit_jet_ES_down) \
     VAR(Int_t, kinfit_convergence) \
     VAR(Float_t, deltaR_ll) \
     VAR(UInt_t, nFatJets) \
@@ -238,8 +262,10 @@ INITIALIZE_TREE(htt_sync, SyncTuple, SYNC_DATA)
 #define COND_VAL_INT(cond, val) cond ? static_cast<int>(val) : default_int_value
 
 namespace htt_sync {
+    
 
     void FillSyncTuple(analysis::EventInfoBase& event, htt_sync::SyncTuple& sync, analysis::Period run_period,
+                       analysis::mva_study::MvaReader* mva_reader = nullptr,
                        analysis::EventInfoBase* event_tau_up = nullptr,
                        analysis::EventInfoBase* event_tau_down = nullptr,
                        analysis::EventInfoBase* event_jet_up = nullptr,
@@ -434,6 +460,11 @@ namespace htt_sync {
                                                event_jet_down->GetKinFitResults().HasValidMass(),
                                                event_jet_down->GetKinFitResults().mass);
 
+        
+        sync().mva_score_nonRes_kl1 = COND_VAL(mva_reader, mva_reader->Evaluate(analysis::mva_study::MvaReader::MvaKey{"mva_smANkin_BSMklscan", 125, 1}, &event));
+        sync().mva_score_lm_320 = COND_VAL(mva_reader, mva_reader->Evaluate(analysis::mva_study::MvaReader::MvaKey{"mva_lmANkin", 320, 0}, &event));
+        sync().mva_score_mm_400 = COND_VAL(mva_reader, mva_reader->Evaluate(analysis::mva_study::MvaReader::MvaKey{"mva_mmANkin", 400, 0}, &event));
+        sync().mva_score_hm_650 = COND_VAL(mva_reader, mva_reader->Evaluate(analysis::mva_study::MvaReader::MvaKey{"mva_hmANkin", 650, 0}, &event));
 
         sync().deltaR_ll = ROOT::Math::VectorUtil::DeltaR(event->p4_1, event->p4_2);
 
@@ -543,6 +574,10 @@ namespace htt_sync {
                                               event_tau_up->GetVBFJet(1).GetMomentum().Pt());
         sync().jpt_tau_ES_up_vbf_2 = COND_VAL(event_tau_up && event_tau_up->HasVBFjetPair(),
                                               event_tau_up->GetVBFJet(2).GetMomentum().Pt());
+        sync().mva_score_nonRes_kl1_tau_ES_up = COND_VAL(event_tau_up && mva_reader, mva_reader->Evaluate(analysis::mva_study::MvaReader::MvaKey{"mva_smANkin_BSMklscan", 125, 1}, event_tau_up));
+        sync().mva_score_lm_320_tau_ES_up = COND_VAL(event_tau_up && mva_reader, mva_reader->Evaluate(analysis::mva_study::MvaReader::MvaKey{"mva_lmANkin", 320, 0}, event_tau_up));
+        sync().mva_score_mm_400_tau_ES_up = COND_VAL(event_tau_up && mva_reader, mva_reader->Evaluate(analysis::mva_study::MvaReader::MvaKey{"mva_mmANkin", 400, 0}, event_tau_up));
+        sync().mva_score_hm_650_tau_ES_up = COND_VAL(event_tau_up && mva_reader, mva_reader->Evaluate(analysis::mva_study::MvaReader::MvaKey{"mva_hmANkin", 650, 0}, event_tau_up));
 
         select_jets(event_tau_down);
         sync().jpt_tau_ES_down_1 = COND_VAL(jets_pt20.size() >= 1, jets_pt20.at(0).GetMomentum().Pt());
@@ -555,6 +590,10 @@ namespace htt_sync {
                                                 event_tau_down->GetVBFJet(1).GetMomentum().Pt());
         sync().jpt_tau_ES_down_vbf_2 = COND_VAL(event_tau_down && event_tau_down->HasVBFjetPair(),
                                                 event_tau_down->GetVBFJet(2).GetMomentum().Pt());
+        sync().mva_score_nonRes_kl1_tau_ES_down = COND_VAL(event_tau_down && mva_reader, mva_reader->Evaluate(analysis::mva_study::MvaReader::MvaKey{"mva_smANkin_BSMklscan", 125, 1}, event_tau_down));
+        sync().mva_score_lm_320_tau_ES_down = COND_VAL(event_tau_down && mva_reader, mva_reader->Evaluate(analysis::mva_study::MvaReader::MvaKey{"mva_lmANkin", 320, 0}, event_tau_down));
+        sync().mva_score_mm_400_tau_ES_down = COND_VAL(event_tau_down && mva_reader, mva_reader->Evaluate(analysis::mva_study::MvaReader::MvaKey{"mva_mmANkin", 400, 0}, event_tau_down));
+        sync().mva_score_hm_650_tau_ES_down = COND_VAL(event_tau_down && mva_reader, mva_reader->Evaluate(analysis::mva_study::MvaReader::MvaKey{"mva_hmANkin", 650, 0}, event_tau_down));
 
         select_jets(event_jet_up);
         sync().jpt_jet_ES_up_1 = COND_VAL(jets_pt20.size() >= 1, jets_pt20.at(0).GetMomentum().Pt());
@@ -567,6 +606,10 @@ namespace htt_sync {
                                               event_jet_up->GetVBFJet(1).GetMomentum().Pt());
         sync().jpt_jet_ES_up_vbf_2 = COND_VAL(event_jet_up && event_jet_up->HasVBFjetPair(),
                                               event_jet_up->GetVBFJet(2).GetMomentum().Pt());
+        sync().mva_score_nonRes_kl1_jet_ES_up = COND_VAL(event_jet_up && mva_reader, mva_reader->Evaluate(analysis::mva_study::MvaReader::MvaKey{"mva_smANkin_BSMklscan", 125, 1}, event_jet_up));
+        sync().mva_score_lm_320_jet_ES_up = COND_VAL(event_jet_up && mva_reader, mva_reader->Evaluate(analysis::mva_study::MvaReader::MvaKey{"mva_lmANkin", 320, 0}, event_jet_up));
+        sync().mva_score_mm_400_jet_ES_up = COND_VAL(event_jet_up && mva_reader, mva_reader->Evaluate(analysis::mva_study::MvaReader::MvaKey{"mva_mmANkin", 400, 0}, event_jet_up));
+        sync().mva_score_hm_650_jet_ES_up = COND_VAL(event_jet_up && mva_reader, mva_reader->Evaluate(analysis::mva_study::MvaReader::MvaKey{"mva_hmANkin", 650, 0}, event_jet_up));
 
         select_jets(event_jet_down);
         sync().jpt_jet_ES_down_1 = COND_VAL(jets_pt20.size() >= 1, jets_pt20.at(0).GetMomentum().Pt());
@@ -579,6 +622,10 @@ namespace htt_sync {
                                                 event_jet_down->GetVBFJet(1).GetMomentum().Pt());
         sync().jpt_jet_ES_down_vbf_2 = COND_VAL(event_jet_down && event_jet_down->HasVBFjetPair(),
                                                 event_jet_down->GetVBFJet(2).GetMomentum().Pt());
+        sync().mva_score_nonRes_kl1_jet_ES_down = COND_VAL(event_jet_down && mva_reader, mva_reader->Evaluate(analysis::mva_study::MvaReader::MvaKey{"mva_smANkin_BSMklscan", 125, 1}, event_jet_down));
+        sync().mva_score_lm_320_jet_ES_down = COND_VAL(event_jet_down && mva_reader, mva_reader->Evaluate(analysis::mva_study::MvaReader::MvaKey{"mva_lmANkin", 320, 0}, event_jet_down));
+        sync().mva_score_mm_400_jet_ES_down = COND_VAL(event_jet_down && mva_reader, mva_reader->Evaluate(analysis::mva_study::MvaReader::MvaKey{"mva_mmANkin", 400, 0}, event_jet_down));
+        sync().mva_score_hm_650_jet_ES_down = COND_VAL(event_jet_down && mva_reader, mva_reader->Evaluate(analysis::mva_study::MvaReader::MvaKey{"mva_hmANkin", 650, 0}, event_jet_down));
 
         sync.Fill();
     }
