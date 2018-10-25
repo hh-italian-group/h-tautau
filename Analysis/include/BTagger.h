65104dab40a5bc5c461a0ec0da450ace4241dcec
rbhattacharya04 <rajarshi.bhattacharya@cern.ch>
2018-10-25 11:37:58 +0530

    Update

--- Analysis/include/BTagger.h
+++ Analysis/include/BTagger.h
@@ -3,7 +3,6 @@ This file is part of https://github.com/hh-italian-group/h-tautau. */
 
 #pragma once
 
-// put includes here
 #include "h-tautau/Analysis/include/AnalysisTypes.h"
 #include "h-tautau/Cuts/include/Btag_2017.h"
 #include "h-tautau/Cuts/include/Btag_2016.h"
@@ -44,20 +43,24 @@ public:
                     {DiscriminatorWP::Medium, cuts::btag_2016::CSVv2M},
                     {DiscriminatorWP::Tight, cuts::btag_2016::CSVv2T}}}}};
 
+        if(!working_points.at(period))
+            throw exception("Period %1% is not supported.") % period;
+        if(!working_points.at(period).at(ordering))
+            throw exception("Jet Ordering %1% is not supported.") % ordering;
         if(!working_points.at(period).at(ordering).at(wp))
             throw exception("Working point %1% is not supported.") % wp;
         
-        // need to do checks here that entry exists, otherwise throw an exception.
         cut = working_points.at(period).at(ordering).at(wp);
     }
 
     double BTag(const ntuple::Event& event, size_t jet_index) const
     {
-        // ...
         if(ordering==JetOrdering::DeepCSV) return event.jets_deepCsv_BvsAll.at(jet_index);
         else if (ordering==JetOrdering::CSV) return event.jets_csv.at(jet_index);
         else if(ordering==JetOrdering::DeepFalvour) return (event.jets_deepFlavour_b.at(jet_index) +
                 event.jets_deepFlavour_bb.at(jet_index) + event.jets_deepFlavour_lepb.at(jet_index));
+        else
+            throw exception("Jet Ordering %1% is not supported") % ordering;
     }
 
     double BTag(const ntuple::TupleJet& jet) const
@@ -65,6 +68,8 @@ public:
         if(ordering==JetOrdering::DeepCSV) return jet.deepcsv();
         else if(ordering==JetOrdering::CSV) return jet.csv();
         else if(ordering==JetOrdering::DeepFalvour) return jet.deepFlavour();
+        else
+            throw exception("Jet Ordering %1% is not supported") % ordering;
     }
 
     bool Pass(const ntuple::Event& event, size_t jet_index) const
