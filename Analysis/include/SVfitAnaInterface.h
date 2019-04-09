587a7019b289e4eb72996c5ef27a07ac5eb9ec22
Maria Teresa <grippomariateresa@gmail.com>
2019-04-09 13:50:13 +0200

    Fixed errors in SVFItAnaInterface

--- Analysis/include/SVfitAnaInterface.h
+++ Analysis/include/SVfitAnaInterface.h
@@ -3,10 +3,6 @@ This file is part of https://github.com/hh-italian-group/h-tautau. */
 
 #pragma once
 
-#include "TauAnalysis/ClassicSVfit/interface/ClassicSVfit.h"
-#include "TauAnalysis/ClassicSVfit/interface/MeasuredTauLepton.h"
-#include "TauAnalysis/ClassicSVfit/interface/svFitHistogramAdapter.h"
-
 #include "h-tautau/Core/include/Candidate.h"
 #include "h-tautau/Core/include/TupleObjects.h"
 #include "AnalysisTools/Core/include/RootExt.h"
@@ -25,23 +21,14 @@ struct FitResults {
                    transverseMass_error(std::numeric_limits<double>::lowest()) {}
 };
 
-inline classic_svFit::MeasuredTauLepton CreateMeasuredLepton(const ntuple::TupleLepton& lepton);
-
 class FitProducer {
 public:
-    explicit FitProducer(int _verbosity = 0)
-        : verbosity(_verbosity)
-    {
-        TH1::AddDirectory(false);
-    }
+    FitProducer(int _verbosity = 0);
 
     FitResults Fit(const LeptonCandidate<ntuple::TupleLepton>& first_daughter,
                    const LeptonCandidate<ntuple::TupleLepton>& second_daughter,
                    const MissingET<ntuple::TupleMet>& met) const;
 
-private:
-    FitResults RunAlgorithm(const std::vector<classic_svFit::MeasuredTauLepton>& measured_leptons,
-                            const LorentzVector& met_momentum, const SquareMatrix<2>& met_cov) const;
 
 private:
     int verbosity;
