f0ee9a51ede5a5d053a161bf95466ccb703c6f8e
Maria Teresa Grippo <mgrippo@users.noreply.github.com>
2019-04-10 17:35:44 +0200

    Core/src/EventTuple.cpp: Removed SVfit_Higgs_indexes from branches (#92)
    Added the possibility to exclude the calculation of tauID weight from LeptonWeights and propagated to EventWeights

--- McCorrections/include/EventWeights.h
+++ McCorrections/include/EventWeights.h
@@ -16,7 +16,7 @@ public:
     using ProviderPtr = std::shared_ptr<IWeightProvider>;
     using ProviderMap = std::map<WeightType, ProviderPtr>;
 
-    EventWeights(Period period, JetOrdering jet_ordering, DiscriminatorWP btag_wp, const WeightingMode& mode = {});
+    EventWeights(Period period, JetOrdering jet_ordering, DiscriminatorWP btag_wp, bool applyTauId, const WeightingMode& mode = {});
     ProviderPtr GetProvider(WeightType weightType) const;
 
     template<typename Provider>
