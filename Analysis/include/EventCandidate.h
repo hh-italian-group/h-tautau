f2f8855bbb0931f4a0ebd70a4576321c2f50794e
Maria Teresa <grippomariateresa@gmail.com>
2019-07-26 14:23:29 +0200

    Applied first PR comments

--- Analysis/include/EventCandidate.h
+++ Analysis/include/EventCandidate.h
@@ -4,13 +4,7 @@ This file is part of https://github.com/hh-italian-group/h-tautau. */
 #pragma once
 
 #include "h-tautau/Core/include/EventTuple.h"
-#include "h-tautau/Core/include/SummaryTuple.h"
 #include "h-tautau/Core/include/TupleObjects.h"
-#include "h-tautau/Cuts/include/hh_bbtautau_2017.h"
-#include "h-tautau/Cuts/include/H_tautau_2016_baseline.h"
-#include "h-tautau/Cuts/include/H_tautau_2017_baseline.h"
-#include "h-tautau/Analysis/include/MetFilters.h"
-#include "h-tautau/JetTools/include/BTagger.h"
 #include "h-tautau/Core/include/Candidate.h"
 #include "h-tautau/Core/include/AnalysisTypes.h"
 #include "h-tautau/Analysis/include/TauUncertainties.h"
@@ -18,19 +12,20 @@ This file is part of https://github.com/hh-italian-group/h-tautau. */
 
 namespace analysis {
 
-using analysis::UncertaintySource;
-using analysis::UncertaintyScale;
-
 class EventCandidate {
 public:
-  using LepCandidate = LeptonCandidate<ntuple::TupleLepton>;
-  using LepCollection = std::vector<LepCandidate>;
-  using JetCandidate = Candidate<ntuple::TupleJet>;
-  using JetCollection = std::vector<JetCandidate>;
-  using MET = MissingET<ntuple::TupleMet>;
+    using LepCandidate = LeptonCandidate<ntuple::TupleLepton>;
+    using LepCollection = std::vector<LepCandidate>;
+    using JetCandidate = Candidate<ntuple::TupleJet>;
+    using JetCollection = std::vector<JetCandidate>;
+    using MET = MissingET<ntuple::TupleMet>;
 
-  EventCandidate(const ntuple::Event& _event, UncertaintySource _uncertainty_source,
-    UncertaintyScale _scale, analysis::Period _period);
+    EventCandidate(const ntuple::Event& _event, UncertaintySource _uncertainty_source,
+    UncertaintyScale _scale, Period _period);
+
+    EventCandidate(const EventCandidate& ) = default; //copy constructor
+
+    EventCandidate& operator= ( const EventCandidate& ) = default; //assignment
 
     static void InitializeJecUncertainty(const std::string& file_uncertainty_source)
     {
@@ -47,17 +42,17 @@ private:
     void CreateLeptons();
     void CreateJets();
 
-  ntuple::Event event;
-  UncertaintySource uncertainty_source;
-  UncertaintyScale scale;
-  analysis::Period period;
-  std::shared_ptr<std::vector<ntuple::TupleLepton>> tuple_leptons;
-  std::shared_ptr<std::vector<ntuple::TupleJet>> tuple_jets;
-  std::shared_ptr<ntuple::TupleMet> tuple_met;
-  std::shared_ptr<std::vector<LepCandidate>> lepton_candidates;
-  std::shared_ptr<std::vector<JetCandidate>> jet_candidates;
-  std::shared_ptr<MET> met;
-  static std::shared_ptr<jec::JECUncertaintiesWrapper> jecUncertainties;
+    ntuple::Event event;
+    UncertaintySource uncertainty_source;
+    UncertaintyScale scale;
+    analysis::Period period;
+    std::shared_ptr<std::vector<ntuple::TupleLepton>> tuple_leptons;
+    std::shared_ptr<std::vector<ntuple::TupleJet>> tuple_jets;
+    std::shared_ptr<ntuple::TupleMet> tuple_met;
+    std::shared_ptr<std::vector<LepCandidate>> lepton_candidates;
+    std::shared_ptr<std::vector<JetCandidate>> jet_candidates;
+    std::shared_ptr<MET> met;
+    static std::shared_ptr<jec::JECUncertaintiesWrapper> jecUncertainties;
 };
 
 } // namespace analysis
